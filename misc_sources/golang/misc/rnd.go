ffa7c127ceeb7c6b1fe7aab563c88dcd68b072de
AbhishekKr <abhikumar163@gmail.com>
2015-01-15 19:32:27 +0530

    [golang] rand example for string and int

---
+++ misc_sources/golang/misc/rnd.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println(randomString(10))
+	fmt.Println(randInt(1, 5))
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
