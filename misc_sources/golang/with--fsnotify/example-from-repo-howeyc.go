c758e44da29d48a23d43c8a0938e3c2da2fb162b
AbhishekKr <abhikumar163@gmail.com>
2015-01-15 19:31:35 +0530

    [golang] example of howeyc/fsnotify

---
+++ misc_sources/golang/with--fsnotify/example-from-repo-howeyc.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"github.com/howeyc/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				log.Println("event:", ev)
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Watch("/tmp/testlog")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+	/* ... do stuff ... */
+	watcher.Close()
+}
