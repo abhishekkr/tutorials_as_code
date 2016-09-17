c69a961c31a159b9738efaafe761e38263c1fcf2
AbhishekKr <abhikumar163@gmail.com>
2016-09-17 13:17:02 +0530

    [golang] extending misc/composite golang slice example

--- talks-articles/golang/misc/composites/slicer.go
+++ talks-articles/golang/misc/composites/slicer.go
@@ -54,6 +54,34 @@ func reslice(yarray [10]string) {
 	fmt.Println("len:", len(tmp_slice), "cap:", cap(tmp_slice), " | : =>", tmp_slice)
 }
 
+func join(v1, v2 []int) []int {
+	return append(v1, v2...)
+}
+
+func someAppend() {
+	lemonSlice := make([]int, 2, 5)
+	fmt.Println(lemonSlice)
+	lemonSlice = join(lemonSlice, []int{2, 3})
+	fmt.Println(lemonSlice)
+	lemonSlice = join(lemonSlice, []int{5, 8, 13, 21})
+	fmt.Println(lemonSlice)
+}
+
+func stringSlice() {
+	str := "StringAsASlice"
+	fmt.Println(str[:6], str[6:8], str[8], str[9:])
+	fmt.Println(str[:6], str[6:8], str[8:9], str[9:])
+
+	byt := []byte(str)
+	fmt.Println(byt[8:9], string(byt[8:9]))
+}
+
+func stringSliceClone(v []string) (result []string) {
+	result = make([]string, len(v), cap(v))
+	copy(result, v)
+	return
+}
+
 func main() {
 	fmt.Println("yaraay ~")
 	for idx, item := range yarray {
@@ -72,4 +100,12 @@ func main() {
 	left_slice(warray)
 	right_slice(warray)
 	reslice(yarray)
+	someAppend()
+	stringSlice()
+	another_yarray := stringSliceClone(yarray[:])
+	fmt.Println("another_yarray: ", another_yarray)
+
+	someStr := "someStr"
+	fmt.Println(someStr[:])
+	fmt.Println(someStr[2:5])
 }
