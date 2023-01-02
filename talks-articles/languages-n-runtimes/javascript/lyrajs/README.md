bcde96dffee46151a00f2fbc2e415baba5f711d5
AbhishekKr <abhikumar163@gmail.com>
2023-01-02 22:19:24 +0530

    [js] add LyraJS sample usage for full-text search
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ talks-articles/languages-n-runtimes/javascript/lyrajs/README.md
@@ -0,0 +1,12 @@
+
+## LyraJS
+
+> [source](https://lyrajs.io/)
+>
+> * Install as `npm install @lyrasearch/lyra`.
+>
+> * [Lyra](https://lyrajs.io) is a dependency-free full-text search engine written in TypeScript. Thus it runs wherever JavaScript can; on a server, edge devices & in brwosers. Focus on Speed. Licesned `Apache 2.0`, OpenSource.
+
+* [Sample Usage](lyra-blog.js)
+
+---
---
+++ talks-articles/languages-n-runtimes/javascript/lyrajs/lyra-blog.js
@@ -0,0 +1,78 @@
+import { create, insert, search } from '@lyrasearch/lyra';
+
+import { createRequire } from 'module'; // to be able to use require at newer node
+const require = createRequire(import.meta.url);
+
+/*
+ * Create Data Schema
+ */
+
+
+const blogDB = create({
+  schema: {
+    title: 'string',
+    file: 'string',
+    tags: 'string',
+    createdAt: 'string',
+  }
+});
+
+/*
+ * Insert/Add Data
+ */
+
+const insertBlog = (b) => {
+  insert(blogDB, {
+    title: b['blogTitle'],
+    file: b['blogFile'],
+    tags: b['blogTags'],
+    createdAt: b['blogDate']
+  });
+}
+
+
+// if not already done, get json file 'curl -LkO https://abhishekkr.github.io/datum.json'
+var fs = require('fs');
+var bloglist = JSON.parse(fs.readFileSync('datum.json', 'utf8'));
+
+bloglist.forEach(
+  function(data, index) {
+    insertBlog(data);
+  }
+)
+
+/*
+ * Search
+ */
+
+const showBlog = (s) => {
+  console.log("Hits: ", s.hits.length)
+  if (s.hits.length > 0) {
+    console.log("[first hit]")
+    console.log(s.hits[0].document);
+  }
+}
+
+const searchPuppet = search(blogDB, {
+  term: "puppet",
+  properties: "*",
+  tolerance: 0,
+});
+console.log("Search: puppet");
+showBlog(searchPuppet);
+
+const searchHowTo = search(blogDB, {
+  term: "how to",
+  properties: ["tags"],
+  tolerance: 1,
+});
+console.log("Search: how to");
+showBlog(searchHowTo);
+
+const searchHowto = search(blogDB, {
+  term: "howto",
+  properties: ["tags"],
+  tolerance: 1,
+});
+console.log("Search: howto");
+showBlog(searchHowto);
