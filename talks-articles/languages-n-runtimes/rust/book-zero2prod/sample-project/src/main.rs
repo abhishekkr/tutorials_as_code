416160e40862b204808eddbfb8a5ad3edf1bcd38
AbhishekKr <abhikumar163@gmail.com>
2023-04-25 04:54:25 +0530

    [rust] notes book:0toProd chap2, chap3 upto greet api
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- talks-articles/languages-n-runtimes/rust/book-zero2prod/sample-project/src/main.rs
+++ talks-articles/languages-n-runtimes/rust/book-zero2prod/sample-project/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("Guest");
+    format!("Hi {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
