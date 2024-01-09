0791ceadc61f7e38078d31505b837bd4a12cecb6
AbhishekKr <abhikumar163@gmail.com>
2024-01-10 03:13:47 +0530

    [fast-api] cha-t-ter: db migrations with Alembic w/ autogen
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- talks-articles/frameworks/fast-api-py/cha-t-ter/lib/dbmgr.py
+++ talks-articles/frameworks/fast-api-py/cha-t-ter/lib/dbmgr.py
@@ -21,12 +21,12 @@ class Users(BASE):
    __tablename__ = 'users'
    id = Column(Integer, primary_key=True, autoincrement=True)
    name = Column(String(50), unique=True, nullable=False)
+   email = Column(String(255), unique=True, nullable=False)
    subscribed = Column(Boolean)
 
 
 class DB:
     def get_db():
-        BASE.metadata.create_all(bind=ENGINE)
         d = SESSION()
         try:
             yield d
