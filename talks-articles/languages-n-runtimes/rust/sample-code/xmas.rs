9c16cbf5172887f47817ddbcb982a9cbc6de3b5c
AbhishekKr <abhikumar163@gmail.com>
2024-12-27 03:50:49 +0530

    [rust] UPDATE print blinking xmas tree in terminal
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- talks-articles/languages-n-runtimes/rust/sample-code/xmas.rs
+++ talks-articles/languages-n-runtimes/rust/sample-code/xmas.rs
@@ -1,7 +1,8 @@
 use std::{thread, time::Duration};
 
 const SLEEP_FOR: Duration = Duration::from_millis(500);
-const TREE_WIDTH: usize = 35;
+const TREE_WIDTH: usize = 40;
+const CLEAR_CODE: &str = "\x1B[2J";
 const COLOR_SYSTEM: &str = "\x1b[0m";
 const COLOR_RED: &str = "\x1b[31m";
 const COLOR_GREEN: &str = "\x1b[32m";
@@ -13,61 +14,42 @@ const BGCOLOR_BLUE: &str = "\x1b[44m";
 const COLOR_YELLOWB: &str = "\x1b[1m\x1b[33m";
 
 fn main() {
-    print!("\x1B[2J");
+    print!("{CLEAR_CODE}");
     let mut color_flag: bool = true;
     loop {
         if color_flag {
-            print_xmas_tree(COLOR_YELLOW, BGCOLOR_BLUE);
+            print_tree(BGCOLOR_RED, COLOR_YELLOW, COLOR_BLUE);
         } else {
-            print_xmas_tree(COLOR_YELLOWB, COLOR_BLUE);
+            print_tree(COLOR_RED, BGCOLOR_YELLOW, BGCOLOR_BLUE);
         }
         thread::sleep(SLEEP_FOR);
-        print!("\x1B[2J");
+        print!("{CLEAR_CODE}");
         color_flag = !color_flag;
     }
 }
 
-fn print_xmas_tree(light_color: &str, trail_color: &str) {
-    let msg = format!("M{COLOR_GREEN}erry{COLOR_SYSTEM}C{COLOR_RED}hri{COLOR_SYSTEM}{COLOR_YELLOW}st{COLOR_SYSTEM}{trail_color}mas{COLOR_SYSTEM}.");
-
-    // print the greeting
+fn print_tree(star_color: &str, trail_color: &str, trunk_color: &str) {
     let mut msg_spacing: usize = TREE_WIDTH;
-    if TREE_WIDTH > 10 {
-        msg_spacing -= 7;
+    if msg_spacing > 10 {
+        msg_spacing /= 2;
     }
-    println!("{}{}{}{}", "\n", " ".repeat(msg_spacing), msg, "\n");
+    let msg = format!("{COLOR_GREEN}Merry {COLOR_SYSTEM}{COLOR_RED}Christmas {COLOR_SYSTEM}{COLOR_YELLOW}and {COLOR_SYSTEM}{COLOR_BLUE}Happy {COLOR_SYSTEM}{COLOR_YELLOWB}Holidays{COLOR_SYSTEM}\n");
+    println!("{}{}", " ".repeat(msg_spacing), msg);
 
-    // print the leaves
-    let xtop = format!("{BGCOLOR_RED}@{COLOR_SYSTEM}{COLOR_RED}X{COLOR_SYSTEM}");
-    let star = format!("{light_color}*{COLOR_SYSTEM}");
-    println!("{}{}", " ".repeat(TREE_WIDTH), xtop);
+    let xstar = format!("{star_color}*{COLOR_SYSTEM}");
+    println!("{}{COLOR_RED}X.{COLOR_SYSTEM}", " ".repeat(TREE_WIDTH));
     for n in (1..TREE_WIDTH).rev() {
-        let fslash = format!(
-            "{}{}{}",
-            COLOR_GREEN,
-            "/".repeat(TREE_WIDTH - n),
-            COLOR_SYSTEM
-        );
-        let bslash = format!(
-            "{}{}{}",
-            COLOR_GREEN,
-            "\\".repeat(TREE_WIDTH - n),
-            COLOR_SYSTEM
-        );
-        println!("{}{}{}{}{}", " ".repeat(n), star, fslash, bslash, star);
+        let fslash = format!("{COLOR_GREEN}{}{COLOR_SYSTEM}", "/".repeat(TREE_WIDTH - n));
+        let bslash = format!("{COLOR_GREEN}{}{COLOR_SYSTEM}", "\\".repeat(TREE_WIDTH - n));
+        let spacing = " ".repeat(n);
+        println!("{}{xstar}{}{}{xstar}", spacing, fslash, bslash);
     }
-    let trails = format!(
-        "{}{}{}",
-        trail_color,
-        ";".repeat(TREE_WIDTH * 2),
-        COLOR_SYSTEM
-    );
-    println!("{}{}{}", star, trails, star);
 
-    //print out the trunk
-    let trunka = format!("{}{}{}", BGCOLOR_YELLOW, "||", COLOR_SYSTEM);
+    let trails = format!("{trail_color}{}{COLOR_SYSTEM}", ";".repeat(TREE_WIDTH * 2));
+    println!("{xstar}{}{xstar}", trails);
+
+    let trunk = format!("{trunk_color}||{COLOR_SYSTEM}");
     for _n in 1..(TREE_WIDTH / 10) {
-        println!("{}{}", " ".repeat(TREE_WIDTH), trunka);
+        println!("{}{}", " ".repeat(TREE_WIDTH), trunk);
     }
-    println!("{}{}{}", " ".repeat(TREE_WIDTH), trunka, "\n");
 }
